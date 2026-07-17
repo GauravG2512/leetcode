ee83a675b0f48942f97e1c9a51fe8e83c52e2281
Gaurav Ghude <144281753+GauravG2512@users.noreply.github.com>
2026-07-17 19:21:44 +0530

    Time: 3 ms (71.77%) | Memory: 8.5 MB (65.44%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.c
+++ 1013-fibonacci-number/fibonacci-number.c
@@ -1,11 +1,12 @@
 int fib(int n){
-    if(n == 0 ){
-        return 0;
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+
+    int num1 = 1, num2 = 1, res = 1;
+    for(int i = 0; i < n - 2; i++){
+        res = num1 + num2;
+        num1 = num2;
+        num2 = res;
     }
-    else if(n == 1){
-        return 1;
-    }
-    else{
-        return fib(n-1)+fib(n-2);
-    }
+    return res;
 }
